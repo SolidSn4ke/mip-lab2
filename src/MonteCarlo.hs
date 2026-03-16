575ecd587cc1539a4ffe7d5d201ad9c7160ca717
SolidSn4ke <artsuper472@gmail.com>
2026-03-16 12:00:46 +0300

    add custom sampling

--- src/MonteCarlo.hs
+++ src/MonteCarlo.hs
@@ -3,16 +3,16 @@ module MonteCarlo (simpleMonteCarlo, stratifiedMonteCarlo, importanceMonteCarlo)
 import Control.Monad (replicateM)
 import System.Random (randomRIO)
 
-simpleMonteCarlo :: (Double -> Double) -> (Double, Double) -> IO Double
-simpleMonteCarlo f (a, b) = do
-    values <- replicateM 100000 (randomRIO (a, b))
-    return $ (*) ((b - a) / 100000) $ sum $ map f values
+simpleMonteCarlo :: (Double -> Double) -> (Double, Double) -> Int -> IO Double
+simpleMonteCarlo f (a, b) n = do
+    values <- replicateM n (randomRIO (a, b))
+    return $ (*) ((b - a) / fromIntegral n) $ sum $ map f values
 
-stratifiedMonteCarlo :: (Double -> Double) -> (Double, Double) -> Double -> IO Double
-stratifiedMonteCarlo f (a, b) step = sum <$> sequence [simpleMonteCarlo f (a', a' + step) | a' <- [a, a + step .. b - step]]
+stratifiedMonteCarlo :: (Double -> Double) -> (Double, Double) -> Double -> Int -> IO Double
+stratifiedMonteCarlo f (a, b) step n = sum <$> sequence [simpleMonteCarlo f (a', a' + step) n | a' <- [a, a + step .. b - step]]
 
-importanceMonteCarlo :: (Double -> Double) -> (Double, Double) -> (Double -> Double) -> IO Double
-importanceMonteCarlo f (a, b) p = do
-    int <- simpleMonteCarlo p (a, b)
-    xs <- replicateM 100000 (randomRIO (a, b))
-    return $ (*) (1 / 100000) $ sum [f x / (p x / int) | x <- xs]
+importanceMonteCarlo :: (Double -> Double) -> (Double, Double) -> (Double -> Double) -> Int -> IO Double
+importanceMonteCarlo f (a, b) p n = do
+    int <- simpleMonteCarlo p (a, b) n
+    xs <- replicateM n (randomRIO (a, b))
+    return $ (*) (1 / fromIntegral n) $ sum [f x / (p x / int) | x <- xs]
