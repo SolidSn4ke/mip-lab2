5b22a7f3a9d98d4cd0831d18c1a66c2abc518d77
SolidSn4ke <artsuper472@gmail.com>
2026-03-16 11:53:11 +0300

    add importance sampling monte carlo

--- src/MonteCarlo.hs
+++ src/MonteCarlo.hs
@@ -1,4 +1,4 @@
-module MonteCarlo (simpleMonteCarlo, stratifiedMonteCarlo) where
+module MonteCarlo (simpleMonteCarlo, stratifiedMonteCarlo, importanceMonteCarlo) where
 
 import Control.Monad (replicateM)
 import System.Random (randomRIO)
@@ -10,3 +10,9 @@ simpleMonteCarlo f (a, b) = do
 
 stratifiedMonteCarlo :: (Double -> Double) -> (Double, Double) -> Double -> IO Double
 stratifiedMonteCarlo f (a, b) step = sum <$> sequence [simpleMonteCarlo f (a', a' + step) | a' <- [a, a + step .. b - step]]
+
+importanceMonteCarlo :: (Double -> Double) -> (Double, Double) -> (Double -> Double) -> IO Double
+importanceMonteCarlo f (a, b) p = do
+    int <- simpleMonteCarlo p (a, b)
+    xs <- replicateM 100000 (randomRIO (a, b))
+    return $ (*) (1 / 100000) $ sum [f x / (p x / int) | x <- xs]
