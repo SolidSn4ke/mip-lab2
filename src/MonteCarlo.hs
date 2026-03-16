3c0960192366ead4b5176fb1f4a5e9944534c703
Artemii Kuzmin <artsuper472@gmail.com>
2026-03-16 16:34:17 +0300

    fix multi importance

--- src/MonteCarlo.hs
+++ src/MonteCarlo.hs
@@ -23,5 +23,9 @@ multiImportanceMonteCarlo f (a, b) p1 p2 n = do
     int2 <- simpleMonteCarlo p2 (a, b) n
     let p1' x = p1 x / int1
     let p2' x = p2 x / int2
+    let w1 x = p1' x / (p1' x + p2' x) 
+    let w2 x = p2' x / (p1' x + p2' x) 
+    let w1' x = p1' x ** 2 / (p1' x ** 2 + p2' x ** 2) 
+    let w2' x = p2' x ** 2 / (p1' x ** 2 + p2' x ** 2) 
     xs <- replicateM n $ randomRIO (a, b)
-    return $ (\(sum1, sum2) -> (sum1 / fromIntegral n, sum2 / fromIntegral n)) $ foldl (\(old1, old2) x -> (old1 + f x / (p1' x + p2' x), old2)) (0, 0) xs
+    return $ (\(sum1, sum2) -> (sum1 / fromIntegral n, sum2 / fromIntegral n)) $ foldl (\(old1, old2) x -> (old1 + w1 x * f x / p1' x + w2 x * f x / p2' x, old2 + w1' x * f x / p1' x + w2' x * f x / p2' x )) (0, 0) xs
