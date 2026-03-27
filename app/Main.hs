baeb346f7932667f3ddf16be369a3c349fb602a2
SolidSn4ke <artsuper472@gmail.com>
2026-03-27 23:01:13 +0300

    add beautiful output

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,87 @@
 module Main (main) where
 
-import Lib
+import MonteCarlo (importanceMonteCarlo, multiImportanceMonteCarlo, russianRoulette, simpleMonteCarlo, stratifiedMonteCarlo)
+import Text.Printf (printf)
 
 main :: IO ()
-main = someFunc
+main = do
+    let ns = [100 :: Int, 1000, 10000, 100000]
+    let f x = x ** 2
+    let interval = (2, 5)
+    let trueVal = 39 :: Double
+    let absErr = abs . (-) 39
+    let estErr x = trueVal / sqrt (fromIntegral x)
+
+    putStrLn "Simple Monte Carlo"
+    putStrLn "Размрер выборки | Расчитанное значение интеграла | Абсолютная ошибка | Оценка погрешности"
+    mapM_
+        ( \n -> do
+            int <- simpleMonteCarlo f interval n
+            putStrLn $ printf "%15d | %30.3f | %17.3f | %18.3f" n int (absErr int) (estErr n)
+        )
+        ns
+
+    putStrLn ""
+    putStrLn "Stratified Monte Carlo"
+    putStrLn "Размрер выборки |  Шаг  | Расчитанное значение интеграла | Абсолютная ошибка | Оценка погрешности"
+    mapM_
+        ( \n -> do
+            mapM_
+                ( \step -> do
+                    int <- stratifiedMonteCarlo f interval step n
+                    putStrLn $ printf "%15d | %5.2f | %30.3f | %17.3f | %18.3f" n step int (absErr int) (estErr n)
+                )
+                [0.5 :: Double, 1]
+        )
+        ns
+
+    putStrLn ""
+    putStrLn "Importance Monte Carlo"
+    putStrLn "Размрер выборки | Функция плотности | Расчитанное значение интеграла | Абсолютная ошибка | Оценка погрешности"
+    mapM_
+        ( \n -> do
+            mapM_
+                ( \p -> do
+                    int <- importanceMonteCarlo f interval p n
+                    let funcToStr
+                            | p 2 == 2 = " x "
+                            | p 2 == 4 = "x^2"
+                            | p 2 == 8 = "x^3"
+                            | otherwise = error ""
+                    putStrLn $ printf "%15d | %17s | %30.3f | %17.3f | %18.3f" n funcToStr int (absErr int) (estErr n)
+                )
+                [id, \x -> (x :: Double) ** 2, \x -> (x :: Double) ** 3]
+        )
+        ns
+
+    putStrLn ""
+    putStrLn "Multi Importance Monte Carlo"
+    putStrLn "Выборка | Плотность 1 | Плотность 2 | Интеграл (ср. плотность) | Интеграл (ср. квадрат плотности) | Абс. ошибка (ср. плотность) | Абс. ошибка (ср. квадрат плотности) | Оценка погрешности"
+    mapM_
+        ( \n -> do
+            mapM_
+                ( \(p1, p2) -> do
+                    (int1, int2) <- multiImportanceMonteCarlo f interval p1 p2 n
+                    let funcToStr p
+                            | p 2 == 2 = " x "
+                            | p 2 == 4 = "x^2"
+                            | p 2 == 8 = "x^3"
+                            | otherwise = error ""
+                    putStrLn $ printf "%7d | %11s | %11s | %24.3f | %32.3f | %27.3f | %35.3f | %18.3f" n (funcToStr p1) (funcToStr p2) int1 int2 (absErr int1) (absErr int2) (estErr n)
+                )
+                [(id, id), (id, \x -> (x :: Double) ** 2), (id, \x -> (x :: Double) ** 3), (\x -> (x :: Double) ** 2, \x -> (x :: Double) ** 2), (\x -> (x :: Double) ** 2, \x -> (x :: Double) ** 3), (\x -> (x :: Double) ** 3, \x -> (x :: Double) ** 3)]
+        )
+        ns
+
+    putStrLn "Russian Roulette Monte Carlo"
+    putStrLn "Размрер выборки | Отсечка | Расчитанное значение интеграла | Абсолютная ошибка | Оценка погрешности"
+    mapM_
+        ( \n -> do
+            mapM_
+                ( \a -> do
+                    int <- russianRoulette f interval a n
+                    putStrLn $ printf "%15d | %7.2f | %30.3f | %17.3f | %18.3f" n a int (absErr int) (estErr n)
+                )
+                [0.5, 0.75, 0.95]
+        )
+        ns
--- src/MonteCarlo.hs
+++ src/MonteCarlo.hs
@@ -1,4 +1,4 @@
-module MonteCarlo (simpleMonteCarlo, stratifiedMonteCarlo, importanceMonteCarlo, multiImportanceMonteCarlo, russianRoulette) where
+module MonteCarlo (simpleMonteCarlo, importanceMonteCarlo, multiImportanceMonteCarlo, russianRoulette, stratifiedMonteCarlo) where
 
 import Control.Monad (replicateM)
 import System.Random (randomRIO)
@@ -23,20 +23,23 @@ multiImportanceMonteCarlo f (a, b) p1 p2 n = do
     int2 <- simpleMonteCarlo p2 (a, b) n
     let p1' x = p1 x / int1
     let p2' x = p2 x / int2
-    let w1 x = p1' x / (p1' x + p2' x) 
-    let w2 x = p2' x / (p1' x + p2' x) 
-    let w1' x = p1' x ** 2 / (p1' x ** 2 + p2' x ** 2) 
-    let w2' x = p2' x ** 2 / (p1' x ** 2 + p2' x ** 2) 
+    let w1 x = p1' x / (p1' x + p2' x)
+    let w2 x = p2' x / (p1' x + p2' x)
+    let w1' x = p1' x ** 2 / (p1' x ** 2 + p2' x ** 2)
+    let w2' x = p2' x ** 2 / (p1' x ** 2 + p2' x ** 2)
     xs <- replicateM n $ randomRIO (a, b)
-    return $ (\(sum1, sum2) -> (sum1 / fromIntegral n, sum2 / fromIntegral n)) $ foldl (\(old1, old2) x -> (old1 + w1 x * f x / p1' x + w2 x * f x / p2' x, old2 + w1' x * f x / p1' x + w2' x * f x / p2' x )) (0, 0) xs
+    return $ (\(sum1, sum2) -> (sum1 / fromIntegral n, sum2 / fromIntegral n)) $ foldl (\(old1, old2) x -> (old1 + w1 x * f x / p1' x + w2 x * f x / p2' x, old2 + w1' x * f x / p1' x + w2' x * f x / p2' x)) (0, 0) xs
 
 russianRoulette :: (Double -> Double) -> (Double, Double) -> Double -> Int -> IO Double
 russianRoulette f (a, b) prob n = do
     values <- replicateM n $ randomRIO (a, b)
-    mapM (\x -> do
-        rand <- randomRIO (0 :: Double, 1)
-        if rand < prob 
-            then
-                return $ f x / prob
-            else return 0
-        ) values >>= (\x -> return $ (b - a) * sum x / fromIntegral n)
+    mapM
+        ( \x -> do
+            rand <- randomRIO (0 :: Double, 1)
+            if rand < prob
+                then
+                    return $ f x / prob
+                else return 0
+        )
+        values
+        >>= (\x -> return $ (b - a) * sum x / fromIntegral n)
